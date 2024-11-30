8b984dae7606ba9b0a026eab741073b34a0bdada
asifurrahaman754 <asifurrahaman754@gmail.com>
2024-11-30 22:47:51 +0600

    fix login form layout

--- src/components/AuthFormWrapper/index.tsx
+++ src/components/AuthFormWrapper/index.tsx
@@ -1,53 +1,46 @@
-import { Box, Grid2 as Grid, Typography } from "@mui/material";
+import { Grid2 as Grid, Typography } from "@mui/material";
 import { ReactNode } from "react";
 import { cssColor } from "utils/colors";
 
 export default function AuthFormWrapper({ children }: { children: ReactNode }) {
   return (
-    <Grid container maxWidth="800px" mx="auto" spacing={4}>
-      <Grid
-        size={{ xs: 12, lg: 6 }}
-        display="grid"
-        sx={{
-          placeItems: "center",
-        }}
-      >
-        <Box width="100%" height="max-content" my="auto">
-          <Typography variant="h1" mb={1}>
-            Track, Resolve, Succeed –<br /> ErrorSnap!
-          </Typography>
-          <Typography variant="caption">
-            ErrorSnap helps you pinpoint problems, resolve them faster, and
-            ensure your app runs flawlessly. Track smarter, resolve quicker, and
-            succeed effortlessly!
-          </Typography>
-        </Box>
+    <Grid
+      minHeight="100vh"
+      alignItems="center"
+      alignContent="center"
+      container
+      maxWidth={{ lg: "800px", xs: "100%" }}
+      mx="auto"
+      spacing={{ xs: 2, lg: 4 }}
+      p={[3, 1]}
+    >
+      <Grid size={{ xs: 12, lg: 6 }}>
+        <Typography variant="h1" mb={1} textAlign="center">
+          Track, Resolve, Succeed –<br /> ErrorSnap!
+        </Typography>
+        <Typography display="block" variant="caption" textAlign="center">
+          ErrorSnap helps you pinpoint problems, resolve them faster, and ensure
+          your app runs flawlessly. Track smarter, resolve quicker, and succeed
+          effortlessly!
+        </Typography>
       </Grid>
       <Grid size={{ xs: 12, lg: 6 }}>
-        <Box
-          width="100%"
-          height="100vh"
-          display="grid"
+        <Grid
           sx={{
-            placeItems: "center",
+            border: "1px solid #1f1f1f",
+            borderRadius: 2,
+            color: "white",
+            backgroundColor: cssColor("paper"),
+            mx: { xs: "auto" },
           }}
+          container
+          spacing={2}
+          px={3}
+          py={3}
+          maxWidth="400px"
         >
-          <Grid
-            sx={{
-              border: "1px solid #1f1f1f",
-              borderRadius: 2,
-              color: "white",
-              backgroundColor: cssColor("paper"),
-            }}
-            container
-            spacing={2}
-            px={3}
-            py={3}
-            maxWidth="400px"
-          >
-            {children}
-          </Grid>
-        </Box>
+          {children}
+        </Grid>
       </Grid>
     </Grid>
   );
